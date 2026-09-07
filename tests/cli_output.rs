b8c590e8adf8cd3b12d37a46c1ba1f046c7089ad
Randolf Jung <randolf@mathematic.io>
2026-09-07 06:11:22 -0700

    test(cli): document installation smoke regression

--- tests/cli_output.rs
+++ tests/cli_output.rs
@@ -1,5 +1,9 @@
+#![warn(rust_2018_idioms)]
+
 use std::process::Command;
 
+// https://github.com/mathematic-inc/if-changed/pull/450
+// Installation checks must work before the user opens a Git repository.
 #[test]
 fn cli_help_and_version_work_outside_a_repository() {
     let directory = tempfile::tempdir().unwrap();
